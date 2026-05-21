1f3ab9b37f186b59f5e7abf76ef4b12e380ebf3e
anhnh-ux <anhnh@ngvgroup.vn>
2026-05-21 08:51:30 +0700

    TEST3

--- html/js/HomeContent.jsx
+++ html/js/HomeContent.jsx
@@ -147,8 +147,8 @@ const HomeContent = () => {
 
       <footer id="contact" className="footer">
         <p style={{ margin: 0 }}>
-          © 2026 Shop Demo. Liên hệ: zalo@demostore.vn - Số điện thoại: 0123 456
-          789 789
+          © 2026 Shop Demo. Liên hệ: zalo@demostore.vn - Số điện thoại:
+          0123456789
         </p>
       </footer>
     </div>
